88c0b11db1be4e3b6b3697c2cb2f1f7b81398305
Manono <manonospaces@gmail.com>
2023-08-28 23:34:58 +0300

    task 9

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    total = 0
+    real_num = 0
     for i in range(x):
         try:
-            print(f"{my_list[i]}", end="")
-            total += 1
-        except IndexError:
+            print(my_list[i], end="")
+            real_num += 1
+        except (IndexError, ValueError, TypeError):
             break
     print()
-    return(total)
+    return(real_num)

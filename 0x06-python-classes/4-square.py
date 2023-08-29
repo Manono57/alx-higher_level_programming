1b5392880ed167442d683391b3fdb9c30d706cfd
Manono <manonospaces@gmail.com>
2023-08-30 01:32:21 +0300

    task 5

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 
+"""A module that defines a square """
+
 
 class Square:
     """A class that represents a square"""
 
     def __init__(self, size=0):
-
+        """Initializing this square class
+        Args:
+            size: represnets the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
 
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
@@ -30,8 +38,8 @@ class Square:
 
     def area(self):
         """
-        Calculate area of the a square
-        Returns: size of the square
+        Calculate area of the square
+        Returns: The square of the size
         """
 
         return (self.__size ** 2)
